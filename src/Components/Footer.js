47fcff2a8edff142e295dc9c10fb796432a3c800
Ansh Pathapadu <apathapadu@carahsoft.com>
2024-07-15 16:57:39 -0400

    Test 2

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer className={`footer ${isHome ? 'footer-home' : 'footer-other'}`}>
       <div className="container">
         <p className="footer-text">
-          &copy; Ansh Pathapadu. All Rights Reserved. Designed & Coded by Ansh Pathapadu.
+          &copy; Ansh Pathapadu. All Rights Reserved - Designed & Coded by Ansh Pathapadu.
         </p>
       </div>
     </footer>
