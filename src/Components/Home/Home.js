88b7f4eabb8a92b0a643f2f0e8b5d8deffbae613
Ansh Pathapadu <apathapadu@carahsoft.com>
2024-07-15 17:08:12 -0400

    Changed background image

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,17 @@
 import React from "react";
 import './Home.css';
 import bgVideo from './Movie Scrabble.mp4';
+import bgImage from './projector.jpg';
 
 function Home() {
   
   return (
     <div className="homepage">
-      <video src={bgVideo} autoPlay loop muted className="video-bg" />
+      <img src={bgImage} className="video-bg" />
     </div>
     //test
   );
 }
 
 export default Home;
+//<video src={bgVideo} autoPlay loop muted className="video-bg" />
